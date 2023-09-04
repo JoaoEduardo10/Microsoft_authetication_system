ce8ce741f7be0f8eec67c0e866d1a467deb83f57
João Eduardo <joao.silva@interativobr.com.br>
2023-09-04 20:06:25 -0300

    feat(errors): adding globals errors middleare

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
+import "express-async-errors";
 import express from "express";
 import { router } from "./router";
 import { config } from "dotenv";
 import { configurePassport } from "../config/passport-microsoft-";
 import passport from "passport";
 import session from "express-session";
+import { globalsErrors } from "./middlewares/globals-errors";
 
 const app = express();
 config();
@@ -23,5 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/v1", router);
+
+app.use(globalsErrors);
 
 export { app };
