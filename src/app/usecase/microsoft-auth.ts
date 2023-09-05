e2b3492cb904a328753400c1100fe3d5aca9d067
João Eduardo <joao.silva@interativobr.com.br>
2023-09-05 10:15:16 -0300

    refactory(microsoft-auth): Adjusting the usecase redirect

--- src/app/usecase/microsoft-auth.ts
+++ src/app/usecase/microsoft-auth.ts
@@ -1,15 +1,19 @@
 import { Request, Response } from "express";
 import { MicrosoftAuthController } from "../controllers/microsoft-auth";
+import { RedisMicrosoftAuthRepository } from "../repositories/microsoft-auth";
 
 class MicrosoftAuthRouter {
   async logged(req: Request, res: Response) {
-    const microsoftAuthController = new MicrosoftAuthController();
+    const redisMicrosoftAuthRepository = new RedisMicrosoftAuthRepository();
+    const microsoftAuthController = new MicrosoftAuthController(
+      redisMicrosoftAuthRepository
+    );
 
-    await microsoftAuthController.handle(req);
+    const { body } = await microsoftAuthController.handle(req);
 
     const url_redirect = process.env.URL_REDIRECT_SUCESS || "";
 
-    res.redirect(url_redirect);
+    res.redirect(`${url_redirect}${body}`);
   }
 }
 
