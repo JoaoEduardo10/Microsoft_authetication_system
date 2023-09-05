5ec38d90db36a9a4354c0023bb1f31ee00bb340f
João Eduardo <joao.silva@interativobr.com.br>
2023-09-05 16:52:05 -0300

    feat(get-microsoft-auth): created controller

---
+++ src/app/controllers/get-microsoft-auth.ts
@@ -0,0 +1,31 @@
+import { createJwt } from "../helpers/jsonwebtoken";
+import { IGetMicrosoftAuthRepositoty } from "../repositories/get-microsoft-auth/protocols";
+import { ApiRequest, ApiResponse, IController } from "./protocols";
+
+class GetMicrosoftAuthController implements IController {
+  constructor(
+    private readonly getMicrosoftAuthRepository: IGetMicrosoftAuthRepositoty
+  ) {}
+
+  async handle(req: ApiRequest<unknown>): Promise<ApiResponse<string>> {
+    const id = req.params.id as string;
+
+    const {
+      key: { email, id: valueId, jobTitle, name },
+    } = await this.getMicrosoftAuthRepository.get({ id });
+
+    const token = createJwt({
+      email,
+      id: valueId,
+      jobs: jobTitle,
+      name,
+    });
+
+    return {
+      body: token,
+      statusCode: 200,
+    };
+  }
+}
+
+export { GetMicrosoftAuthController };
